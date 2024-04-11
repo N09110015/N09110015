7f8009609c8faf818e5945bb39bc072aef4f874f
N09110015 <amy0147258@gmail.com>
2024-04-11 21:24:00 +0800

    First Commit

--- N09110015/WindowsFormsApp1/FormTextBox.cs
+++ N09110015/WindowsFormsApp1/FormTextBox.cs
@@ -45,5 +45,50 @@ namespace WindowsFormsApp1
         {
             MessageBox.Show("哈哈");
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            int a, b;
+            try
+            {
+                a = Int32.Parse(textBox3.Text);
+                b = Int32.Parse(textBox4.Text);
+            }
+            catch (System.FormatException)
+
+            {
+                MessageBox.Show("給我數字!");
+                a = 0;
+                b = 0;
+              
+            }
+            label2.Text = (a + b).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int c = Int32.Parse(textBox6.Text);
+            int d = Int32.Parse(textBox5.Text);
+            label7.Text = (c - d).ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int f = Int32.Parse(textBox8.Text);
+            int g = Int32.Parse(textBox7.Text);
+            label10.Text = (f * g).ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int h = Int32.Parse(textBox10.Text);
+            int i = Int32.Parse(textBox9.Text);
+            label13.Text = (h / i).ToString();
+        }
     }
 }
